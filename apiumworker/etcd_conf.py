51f99b44965685a5f1cd1ab353d87c0b2c159d9f
Zephyre <haizi.zh@gmail.com>
2015-06-26 01:20:04 +0800

    优化对etcd配置服务器的支持

--- apiumworker/etcd_conf.py
+++ apiumworker/etcd_conf.py
@@ -1,40 +1,52 @@
-import os
-
+# coding=utf-8
 import requests
 
 
 __author__ = 'zephyre'
 
-etcd_host = os.getenv('ETCD_HOST', 'etcd')
-etcd_port = int(os.getenv('ETCD_PORT', 2379))
-etcd_url = 'http://%s:%d' % (etcd_host, etcd_port)
+
+def get_etcd_host():
+    import os
+
+    val = os.getenv('ETCD_HOST')
+    if val is None:
+        print 'Cannot find environment variable ETCD_HOST, use "etcd" as default.'
+        val = 'etcd'
+    return val
 
 
-def merge_dicts(*dict_args):
-    """
-    Given any number of dicts, shallow copy and merge into a new dict,
-    precedence goes to key value pairs in latter dicts.
-    """
-    result = {}
-    for dictionary in dict_args:
-        result.update(dictionary)
-    return result
+def get_etcd_port():
+    import os
+
+    val = os.getenv('ETCD_PORT')
+    if val is None:
+        print 'Cannot find environment variable ETCD_HOST, use 2379 as default.'
+        val = 2379
+    else:
+        val = int(val)
+    return val
 
 
-def get_service(service_name):
+etcd_url = 'http://%s:%d' % (get_etcd_host(), get_etcd_port())
+
+
+def get_service(service_name, alias):
     url = '%s/v2/keys/backends/%s' % (etcd_url, service_name)
     try:
         nodes = requests.get(url).json()['node']['nodes']
         values = nodes[0]['value'].split(':')
         host = values[0]
         port = int(values[1])
-        return {service_name: {'host': host, 'port': port}}
+        return {alias: {'host': host, 'port': port}}
     except (KeyError, IndexError, ValueError):
         return None
 
 
-def build_conf(node):
-    current_key = node['key'].split('/')[-1]
+_conf_map = {}
+
+
+def build_conf(node, alias=None):
+    current_key = alias or node['key'].split('/')[-1]
     if 'dir' in node and node['dir'] and 'nodes' in node and node['nodes']:
         m = {}
         for n in node['nodes']:
@@ -46,19 +58,70 @@ def build_conf(node):
         return None
 
 
-def get_config(service_names=None, conf_names=None):
-    import requests
+def parse_cl_args():
+    """
+    解析命令行参数
+    """
+    import argparse
+    import sys
 
-    services = merge_dicts(*filter(lambda v: v, [get_service(s) for s in service_names]))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--module', '-m', nargs='*', choices=['sms'])
+    parser.add_argument('--runlevel', choices=['production', 'dev', 'test'])
+    extracted_args, left_over = parser.parse_known_args()
+
+    args = sys.argv[:1]
+    args.extend(left_over)
+
+    return {'modules': extracted_args.module, 'runlevel': extracted_args.runlevel}, args
+
+
+project_conf, cmd_args = parse_cl_args()
+
+
+def get_config(service_names=None, conf_names=None, cache_key=None, force_refresh=False):
+    if not force_refresh and cache_key and cache_key in _conf_map:
+        return _conf_map[cache_key]
+
+    def merge_dicts(*dict_args):
+        """
+        Given any number of dicts, shallow copy and merge into a new dict,
+        precedence goes to key value pairs in latter dicts.
+        """
+        result = {}
+        for dictionary in dict_args:
+            result.update(dictionary)
+        return result
+
+    def build_tuple(val):
+        """
+        如果val形如(name, alias)，则返回(name, alias)
+        如果val为一个字符串，比如service，则返回(service, service)
+        其它情况：抛出ValueError
+        """
+        if isinstance(val, tuple) and len(val) == 2:
+            return val
+        elif isinstance(val, basestring):
+            return val, val
+        else:
+            raise ValueError
+
+    services = merge_dicts(*filter(lambda v: v, [get_service(*build_tuple(entry)) for entry in service_names]))
 
     conf_map = {}
-    for name in conf_names if conf_names is not None else []:
+    for entry in conf_names if conf_names is not None else []:
+        name, alias = build_tuple(entry)
         url = '%s/v2/keys/project-conf/%s?recursive=true' % (etcd_url, name)
         data = requests.get(url).json()
         if 'node' not in data:
             continue
         else:
-            conf_map.update(build_conf(data['node']))
+            conf_map.update(build_conf(data['node'], alias=alias))
 
-    return {'services': services, 'conf': conf_map}
+    config = merge_dicts({'services': services}, conf_map, project_conf)
+
+    if cache_key:
+        _conf_map[cache_key] = config
+
+    return config
 
--- apiumworker/sms/app.py
+++ apiumworker/sms/app.py
@@ -3,14 +3,24 @@ __author__ = 'zephyre'
 
 def __init_app():
     from celery import Celery
-    from apiumworker.etcd_conf import get_config
+    from apiumworker.etcd_conf import get_config, project_conf
 
-    conf = get_config(['smscenter', 'rabbitmq'], ['smscenter', 'rabbitmq'])
+    runlevel = project_conf['runlevel']
+    if runlevel == 'production':
+        apiumworker_name = 'apiumworker'
+    elif runlevel == 'dev':
+        apiumworker_name = 'apiumworker-dev'
+    elif runlevel == 'test':
+        apiumworker_name = 'apiumworker-test'
+    else:
+        raise ValueError
+
+    conf = get_config(['smscenter', 'rabbitmq'], ['smscenter', (apiumworker_name, 'apiumworker')], cache_key='sms')
+    conf_rabbitmq = conf['apiumworker']['rabbitmq']
+    service_rabbitmq = conf['services']['rabbitmq']
 
     from apiumworker.sms import celery_config
 
-    conf_rabbitmq = conf['conf']['smscenter']['rabbitmq']
-    service_rabbitmq = conf['services']['rabbitmq']
     the_app = Celery('ApiumWorker', broker='amqp://%s:%s@%s:%d/%s' %
                                            (conf_rabbitmq['username'], conf_rabbitmq['password'],
                                             service_rabbitmq['host'], service_rabbitmq['port'],
