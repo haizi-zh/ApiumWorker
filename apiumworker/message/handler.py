190076996ca75a3d2d98b3ebd132b6a93c81cb35
Zephyre <haizi.zh@gmail.com>
2015-08-27 11:01:31 +0800

    ADD: 添加乐视app下载二维码的活动

--- apiumworker/message/handler.py
+++ apiumworker/message/handler.py
@@ -3,13 +3,14 @@ __author__ = 'zephyre'
 
 import re
 import json
-from datetime import datetime
 
+from qiniu import Auth
 from celery.utils.log import get_task_logger
+import requests
+
 from apiumworker.message.app import app
 from apiumworker.etcd_conf import get_config
 
-import requests
 
 logger = get_task_logger('apium')
 
@@ -18,6 +19,8 @@ USERID_WENWEN = 10001
 
 hedy_host = get_config(cache_key='apiumworker.contact')['services']['hedy'].values()[0]['host']
 hedy_port = get_config(cache_key='apiumworker.contact')['services']['hedy'].values()[0]['port']
+
+qiniu_client = Auth('jU6KkDZdGYODmrPVh5sbBIkJX65y-Cea991uWpWZ', 'OVSfoBU_Lzb6QgMWCvOD0x1mDO10JxIOwCuIYNr0')
 
 # 接收到消息的事件
 @app.task(serializer='json', name='hedylogos.onFilterMessage')
@@ -50,6 +53,34 @@ def _build_text_message(sender_id, receiver_id, text):
         'receiver': receiver_id,
         'sender': sender_id
     }
+
+
+def _build_image_message(sender_id, receiver_id, **kwargs):
+    """
+    发送图像消息
+    :param sender_id:
+    :param receiver_id:
+    :param text:
+    :return:
+    """
+
+    key = kwargs['key']
+    bucket_domain = kwargs['bucket_domain']
+    base_url = 'http://%s/%s' % (bucket_domain, key)
+
+    style = ['thumb', 'full']
+
+    # 默认一周以后过期
+    expires = 7 * 24 * 3600
+
+    private_urls = {tmp: qiniu_client.private_download_url('%s!%s' % (base_url, tmp), expires=expires) for tmp in style}
+
+    contents = {'width': kwargs['width'], 'height': kwargs['height']}
+    for k, v in private_urls.items():
+        contents[k] = v
+
+    return {'msgType': 2, 'chatType': 'single', 'contents': json.dumps(contents),
+            'receiver': receiver_id, 'sender': sender_id}
 
 
 def _send_message(message):
@@ -90,13 +121,7 @@ def paipai_filter(**kwargs):
     msg_type = kwargs['msgType']
     contents = kwargs['contents'].strip()
 
-    if chat_type == 'single' and msg_type == 0:
-        m = re.search(ur'^我要京东卡.*(1\d{10})', contents)
-        if not m:
-            return
-
-        tel = m.group(1)
-
+    def jd_lottery(tel):
         logger.info(u'Lottery registered for %d: %s' % (sender, tel))
 
         # 注册到etcd服务器中。默认30天过期
@@ -105,10 +130,26 @@ def paipai_filter(**kwargs):
         r = etcd_request(key, method='PUT', data={'value': tel})
         logger.debug(r)
 
-        contents = u'亲爱的，已经收到您参与活动的信息了，派派会在活动结束后3个工作日内通知您中奖结果，' \
-                   u'中奖名单也会在旅行派官方微博中公布，敬请期待哟~么么哒~'
-        msg = _build_text_message(USERID_PAIPAI, sender, contents)
+        new_contents = u'亲爱的，已经收到您参与活动的信息了，派派会在活动结束后3个工作日内通知您中奖结果，' \
+                       u'中奖名单也会在旅行派官方微博中公布，敬请期待哟~么么哒~'
+        msg = _build_text_message(USERID_PAIPAI, sender, new_contents)
         _send_message(msg)
+
+    def letv_qr_code():
+        msg1 = _build_text_message(USERID_PAIPAI, sender, u'乐视永久会员二维码送上，么么哒~')
+        _send_message(msg1)
+
+        key = '31434498-7d4d-4fb5-b7b4-757e01efd5f1'
+        bucket_domain = '7xirnn.com1.z0.glb.clouddn.com'
+        msg2 = _build_image_message(USERID_PAIPAI, sender, key=key, bucket_domain=bucket_domain, width=160, height=160)
+        _send_message(msg2)
+
+    if chat_type == 'single' and msg_type == 0:
+        m = re.search(ur'^我要京东卡.*(1\d{10})', contents)
+        if m:
+            jd_lottery(m.group(1))
+        elif contents.strip() == u'我要码' and sender == 100056:
+            letv_qr_code()
 
 
 def wenwen_filter(**kwargs):
